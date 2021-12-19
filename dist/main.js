33c0d3f6c9ddf7d5a3a60b0d0b69cead9c3bf29a
Gianmarco Marucelli <gianmarcomarucelli@gmail.com>
2021-12-19 12:16:44 +0100

    add better gui

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function RNGDec(min, max, precision) {
     const stringErr = `il numero di precision '${precision}' deve essere un numero intero positivo diverso da 0!!`;
     if (Math.sign(precision) != 1) {
@@ -16,11 +17,61 @@ function RNG(min, max) {
     return Math.trunc(rng * (max - min) + min);
 }
 function RNGSequence(len, min, max) {
+    if (len > max - min) {
+        throw new Error(`non posso trovare ${len} numeri tra ${min} e ${max}`);
+    }
     const res = [];
-    for (let i = 0; i < len; i++) {
-        res.push(RNG(min, max));
+    while (res.length < len) {
+        const rn = RNG(min, max);
+        if (res.includes(rn)) {
+            continue;
+        }
+        res.push(rn);
     }
     return res;
 }
-const myArr = RNGSequence(10, 0, 10);
-console.log(myArr);
+function createRuota(ruotaname, estrazioni) {
+    const ruotadiv = document.createElement("div");
+    ruotadiv.className = `ruota ${ruotaname.toLowerCase()}`;
+    const nameh2 = document.createElement("h2");
+    nameh2.innerText = ruotaname;
+    nameh2.className = "ruota-titolo";
+    ruotadiv.appendChild(nameh2);
+    for (const numb of estrazioni) {
+        const numbP = document.createElement("p");
+        numbP.innerText = "" + numb;
+        const numbDiv = document.createElement("div");
+        numbDiv.className = "ruota-estrazione";
+        numbDiv.appendChild(numbP);
+        ruotadiv.appendChild(numbDiv);
+    }
+    return ruotadiv;
+}
+const ruote = [
+    'BARI',
+    'CAGLIARI',
+    'FIRENZE',
+    'GENOVA',
+    'MILANO',
+    'NAPOLI',
+    'PALERMO',
+    'ROMA',
+    'TORINO',
+    'VENEZIA',
+    'NAZIONALE',
+];
+const estrazioni = {};
+for (const ruota of ruote) {
+    const estrazione = RNGSequence(5, 1, 91);
+    estrazioni[ruota] = estrazione;
+}
+console.log();
+const container = document.getElementById("cnt");
+if (container) {
+    const pre = document.createElement("pre");
+    for (const ruota of ruote) {
+        const ruotaEstrazioni = estrazioni[ruota];
+        const ruotadiv = createRuota(ruota, ruotaEstrazioni);
+        container.appendChild(ruotadiv);
+    }
+}
--- main.ts
+++ main.ts
@@ -32,7 +32,25 @@ function RNGSequence(len: number, min: number, max: number): number[] {
     }
     return res;
 }
- 
+
+function createRuota(ruotaname: string, estrazioni: number[]) {
+    const ruotadiv = document.createElement("div");
+    ruotadiv.className = `ruota ${ruotaname.toLowerCase()}`;
+    const nameh2 = document.createElement("h2");
+    nameh2.innerText = ruotaname;
+    nameh2.className = "ruota-titolo";
+    ruotadiv.appendChild(nameh2);
+
+for (const numb of estrazioni) {
+    const numbP = document.createElement("p");
+    numbP.innerText = "" + numb;
+    const numbDiv = document.createElement("div");
+    numbDiv.className = "ruota-estrazione";
+    numbDiv.appendChild(numbP);
+    ruotadiv.appendChild(numbDiv);
+    }
+    return ruotadiv;
+}
 const ruote = [
     'BARI',
     'CAGLIARI', 
@@ -54,4 +72,15 @@ for(const ruota of ruote) {
     const estrazione = RNGSequence(5, 1, 91);
     estrazioni[ruota] = estrazione;
 }
-console.log(JSON.stringify(estrazioni, null, 2));
+console.log();
+
+const container = document.getElementById("cnt");
+ if (container) {
+     const pre = document.createElement("pre");
+     for (const ruota of ruote) {
+         const ruotaEstrazioni = estrazioni[ruota]
+         const ruotadiv = createRuota(ruota, ruotaEstrazioni);
+         container.appendChild(ruotadiv);
+     }
+     
+ }
